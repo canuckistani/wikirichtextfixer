56969eda5d58e7b6f05ae0d0ccee3937609980cb
canuckistani <jeff@canuckistani.ca>
2013-06-18 08:56:54 -0700

    copied from builder

---
+++ data/content_script.js
@@ -0,0 +1,22 @@
+$(function() {
+
+    function appender(node) {
+        var link = $(node).attr('href');
+        $(node).attr('href', link+"&useskin=vector");
+    }
+
+    $('span.editsection a').each(function() {
+        appender(this);
+    });
+    
+    $('li#ca-edit >a').each(function() {
+        appender(this);
+    });
+    
+    $('div#header > ul > li > a').each(function() {
+        if (this.innerHTML === 'Edit') {
+    		appender(this);
+    	}
+    });
+
+});
