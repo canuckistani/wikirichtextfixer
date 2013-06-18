320a21f477c8ec317ac37014acec4fb883f2263b
canuckistani <jeff@canuckistani.ca>
2013-06-18 09:03:55 -0700

    whitespace, always attach, modern paths

--- data/content_script.js
+++ data/content_script.js
@@ -1,5 +1,4 @@
 $(function() {
-
     function appender(node) {
         var link = $(node).attr('href');
         $(node).attr('href', link+"&useskin=vector");
@@ -8,15 +7,14 @@ $(function() {
     $('span.editsection a').each(function() {
         appender(this);
     });
-    
+
     $('li#ca-edit >a').each(function() {
         appender(this);
     });
-    
+
     $('div#header > ul > li > a').each(function() {
         if (this.innerHTML === 'Edit') {
     		appender(this);
     	}
     });
-
 });
